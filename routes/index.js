30d1e8cc310004ad79c9820d6f499eb34f23ce16
Hansell Kopp <hansellkopp@gmail.com>
2017-07-22 12:28:10 +0200

    =Init database www

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ var models  = require('../models')
 var express = require('express')
 var router  = express.Router()
 
-// Initialize sequelize and drop the database before reconstructing it (the force: true)
-models.sequelize.sync({force: true}).then(function() {
-    console.log('Database Initialized');
-});
 
 router.get('/', function(req, res) {
     res.render('index', {
